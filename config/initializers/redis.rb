1885198cd5a4dbff6e38ac1896ceafd087b9489e
mansimas <magimantas@yahoo.com>
2016-07-02 12:52:15 +0300

    redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,6 @@
-
-uri = ENV["REDISTOGO_URL"] || "redis://localhost:6379/"
-
-$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+if ENV["REDISTOGO_URL"]
+	uri = ENV["REDISTOGO_URL"]
+	$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+	$redis = Redis.new(:host => '127.0.0.1', :port => 6379)
+end
